07f932783a1185afb04f9bcbbb967dbd170aaeca
Xiaoyu006 <46880093+Xiaoyu006@users.noreply.github.com>
2022-06-13 19:08:11 +0800

    Add files via upload

---
+++ visualizations/plot_frame.py
@@ -0,0 +1,25 @@
+"""
+Plot a frame including:
+    1. plot the map
+    2. plot the vehicles in this map
+    3. plot graph structure with directed edges. Edge types should be indicated.
+"""
+
+class IT_Plottor():
+    def __init__(self):
+        pass
+    
+    def plot_hist(self, ax, hist):
+        pass
+
+    def plot_fut(self, ax, fut):
+        pass
+
+    def plot_map(self, map):
+        pass
+
+    def plot_arrow(self, arrow):
+        pass
+
+    def plot_box(self, box):
+        pass
